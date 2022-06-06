fa267c7c2065963f386ffcd0b78ecd893b4e6765
step1121 <au5.iphonex3@outlook.jp>
2022-06-06 09:15:20 +0900

    scope

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -1,4 +1,10 @@
-var nickname = "taro"
-console.log(nickname)
-nickname = "ichiro"
-console.log(nickname)
+var str = "webcamp"
+
+function foo() {
+  var str
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
