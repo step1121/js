208a63d67fd8efb54243586165cfa8cb9b164466
step1121 <au5.iphonex3@outlook.jp>
2022-07-12 10:50:50 +0900

    for

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-let orange = 100;
-let apple = 120;
+let i;
+let num = 0;
 
-if(orange < apple){
-  alert("みかんの値段がリンゴより安い")
-} else if(orange == apple){
-  alert("みかんとりんごが同じ値段")
-} else {
-  alert("みかんの値段がリンゴより高い")
+for(i = 1; i < 11; i++){
+  num = num + i;
 }
 
-let max = 100;
-let num = 1;
-let count = 0;
-
-while(num < max){
-  num = num *2;
-  count = count + 1;
-}
-
-alert("2をかけ続けて" + max + "を超えるのに必要だった回数は" + count + "回です");
+alert("１から10まで足し算した結果は" + num + "です");
